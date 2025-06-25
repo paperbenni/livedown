8438d73590062409a62eb5705abf850e807dd735
paperbenni <paperbenni@gmail.com>
2025-06-25 20:34:51 +0200

    remove dist

--- dist/bin/livedown.d.ts
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bun
-export {};
-//# sourceMappingURL=livedown.d.ts.map
--- dist/utils.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function isPathRelative(p: string): boolean;
-/**
- * Splits cmd on space characters unless they are quoted
- * Example: "'google chrome' --incognito" ==> ['google chrome', '--incognito']
- */
-export declare function splitCommandLine(cmd?: string): string[];
-//# sourceMappingURL=utils.d.ts.map
