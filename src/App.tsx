793e610d3a181fdea825bc4c00a184e3a21c4b38
paperbenni <paperbenni@gmail.com>
2025-06-26 10:26:41 +0200

    init math rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import Markdown from "react-markdown";
 import "./App.css";
+import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
+import { Button } from "./components/ui/button";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 function App() {
   // const [count, setCount] = useState(0);
+  const markdown = `
+  # Hello World
 
+  This is a simple markdown editor.
+
+  ## Features
+
+  - Syntax highlighting
+  - Live preview
+  - Markdown shortcuts
+  $$
+  \\begin{aligned}
+  & x = y + z \\\\
+  & x = y - z \\
+  & x = y \\cdot z \\
+  & x = \\frac{y}{z}
+  \\end{aligned}
+  $$
+  \`\`\`javascript
+  function greet() {
+    console.log("Hello, World!");
+  }
+  greet();
+  \`\`\`
+  `;
   return (
     <>
       <h1 className="text-4xl">Hello</h1>
+      <Button>Hello</Button>
+      <div className="mx-auto max-w-2xl">
+        <div className="prose">
+          <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+            {markdown}
+          </Markdown>
+        </div>
+      </div>
     </>
   );
 }
